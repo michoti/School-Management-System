d3cca60c6eb9c013048365b135fe06d80b2f5178
shukeel <triggaillest@gmail.com>
2022-07-24 15:09:04 +0300

    modified

---
+++ database/factories/ExamFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Exam>
+ */
+class ExamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'type' => $this->faker->numberBetween(100, 400)
+        ];
+    }
+}
