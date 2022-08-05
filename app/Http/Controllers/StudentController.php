d43c65200da52576a68c16de386e8db8d1f55b79
shukeel <triggaillest@gmail.com>
2022-08-05 21:07:50 +0300

    modified

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\StudentResource;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $pageSize = $request->page_size ?? 20;
+
+        $students = Student::query()->paginate($pageSize);
+
+        return StudentResource::collection($students);
+
+
+    }
 }
