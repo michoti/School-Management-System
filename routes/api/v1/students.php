6158bb9c5b6b21e01ed3ebebad756775cf6c374d
shukeel <triggaillest@gmail.com>
2022-08-05 20:35:11 +0300

    modified

---
+++ routes/api/v1/students.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Routing\Route;
+
+Route::middleware()
+    ->name('students.')
+    ->group(function (){
+
+    Route::get('/students', [StudentController::class, 'index'])->name('index');
+
+    Route::get('/students/{student}', [StudentController::class, 'show'])->name('show')->whereNumber('student');
+
+    Route::post('/students', [StudentController::class, 'store'])->name('store');
+
+    Route::put('/students/{student}', [StudentController::class, 'update'])->name('update');
+
+    Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('delete');
+
+});
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 2;
-});
+// Route::get('/', function () {
+//     return 2;
+// });
