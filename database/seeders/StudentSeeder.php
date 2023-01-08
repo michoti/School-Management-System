72c53d4c75fc62c81a5ce1ec86e3a26530f25ce8
shukeel <triggaillest@gmail.com>
2023-01-08 12:57:56 +0300

    modified

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -4,8 +4,11 @@ namespace Database\Seeders;
 
 use App\Models\Classroom;
 use App\Models\Student;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class StudentSeeder extends Seeder
 {
@@ -16,6 +19,24 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        Student::factory(10)->create();        
+        Student::factory(10)->create();   
+        
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin12345'),
+            'role_as' => 'admin',
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'teacher',
+            'email' => 'teacher@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('teacher12345'),
+            'role_as' => 'teacher',
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
